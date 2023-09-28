2f294f95a33f2a8e1c0de20ba85105ad0800c0cd
aminekell <aminekella2003.com>
2023-09-28 15:42:20 -0700

    done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,22 +10,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int total = 0;
-	int len = 0;
 
 	if (b == NULL)
 		return (0);
 
-	/* Calculate the length of the binary string */
-	while (b[len] != '\0')
-	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-		len++;
-	}
-
-	/* Convert binary to decimal */
 	for (int i = 0; b[i] != '\0'; i++)
 	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 		total = total * 2 + (b[i] - '0');
 	}
 
